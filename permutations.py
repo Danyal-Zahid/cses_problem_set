f7d392869a31e57a09c9ac9b80782e95027e8b1e
Danyal-Zahid <danyal.zahid2000@gmail.com>
2024-01-30 21:16:57 +0500

    permutations

---
+++ permutations.py
@@ -0,0 +1,13 @@
+def print_numbers(start, end):
+    while start <= end:
+        print(start, end=" ")
+        start += 2
+
+n = int(input())
+
+if 2 <= n < 4:
+    print("NO SOLUTION")
+else:
+    print_numbers(2, n)
+    print_numbers(1, n)
+
